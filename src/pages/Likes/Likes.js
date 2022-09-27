28abc4a923e0f635449378328ac083e64382b3a4
galee1025 <galee1025@gamil.com>
2022-09-27 10:54:25 +0900

    Add: 삭제 기능 구현

--- src/pages/Likes/Likes.js
+++ src/pages/Likes/Likes.js
@@ -1,118 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LikeItem from './components/LikeItem';
+import LikeNull from './components/LikeNull';
 import './Likes.scss';
 
 const Likes = () => {
-  // const mouseOverImg = e => {
-  //   console.log(e.currentTarget.getAttribute('name'));
-  // };
+  const [likeList, setLikeList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/likeData.json')
+      .then(res => res.json())
+      .then(result => setLikeList(result.data));
+  }, []);
+
+  const deleteBtn = e => {
+    let copy = [...likeList];
+    let copy2 = copy.filter(item => item.id !== parseInt(e.target.id));
+    setLikeList(copy2);
+  };
 
   return (
     <div className="Likes">
       <div className="title-container">
         <h1 className="likes-title">관심상품</h1>
-        <p className="count-likes">나의 관심상품 93</p>
+        <p className="count-likes">나의 관심상품 {likeList.length}</p>
       </div>
 
+      <div className="like-line" />
       <div className="likes-item-container">
-        {LIKE_DATA.map(likeItem => {
-          const { id, name, price, detail_image } = likeItem;
-          const PRICE = Number(price);
-          return (
-            <div
-              key={id}
-              name={name}
-              className="likes-item"
-              // onMouseOver={mouseOverImg}
-            >
-              <div className="img-container">
-                <img src={detail_image} alt="관심상품 이미지" />
-
-                <div className="like-out-btn">x</div>
-              </div>
-
-              <h2 className="likes-item-name">{name}</h2>
-              <p className="likes-item-price">{PRICE.toLocaleString()}원</p>
-            </div>
-          );
-        })}
+        {likeList.length === 0 ? (
+          <LikeNull />
+        ) : (
+          likeList.map(product => {
+            return (
+              <LikeItem
+                key={product.id}
+                id={product.id}
+                likeData={product}
+                deleteBtn={deleteBtn}
+              />
+            );
+          })
+        )}
       </div>
+      <div className="like-line" />
     </div>
   );
 };
 
 export default Likes;
-
-const LIKE_DATA = [
-  {
-    id: 1,
-    name: 'NikeDunkLowPolaroid',
-    price: '150000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)nikedunk1.png?raw=true ',
-    quantity: 1,
-    checkbox: 1,
-  },
-  {
-    id: 2,
-    name: 'NIKEwindbreaker',
-    price: '99000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)windbreaker1.png?raw=true',
-    quantity: 6,
-    checkbox: 1,
-  },
-  {
-    id: 3,
-    name: 'GarminForerunner245',
-    price: '199000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)garminwatch1.png?raw=true',
-    quantity: 1,
-    checkbox: 1,
-  },
-  {
-    id: 4,
-    name: 'Gram',
-    price: '150000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/sub)gram3.png?raw=true ',
-    quantity: 1,
-    checkbox: 1,
-  },
-  {
-    id: 5,
-    name: 'NikeDunkLowPolaroid',
-    price: '150000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)nikedunk1.png?raw=true ',
-    quantity: 1,
-    checkbox: 1,
-  },
-  {
-    id: 6,
-    name: 'NIKEwindbreaker',
-    price: '99000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)windbreaker1.png?raw=true',
-    quantity: 6,
-    checkbox: 1,
-  },
-  {
-    id: 7,
-    name: 'GarminForerunner245',
-    price: '199000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/main)garminwatch1.png?raw=true',
-    quantity: 1,
-    checkbox: 1,
-  },
-  {
-    id: 8,
-    name: 'Gram',
-    price: '150000.0',
-    detail_image:
-      'https://github.com/ChoiRamsey/zinwoos/blob/main/MODIFY-ZINWOOS-PHOTO/sub)gram3.png?raw=true ',
-    quantity: 1,
-    checkbox: 1,
-  },
-];
---
+++ src/pages/Likes/components/LikeItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './LikeItem.scss';
+
+const LikeItem = ({ deleteBtn, likeData }) => {
+  const { id, name, price, detail_image } = likeData;
+
+  const PRICE = Number(price);
+
+  return (
+    <div className="LikeItem">
+      <div className="img-container">
+        <img src={detail_image} alt="관심상품 이미지" />
+        <div className="like-out-btn" id={id} onClick={deleteBtn}>
+          x
+        </div>
+      </div>
+
+      <h2 className="likes-item-name">{name}</h2>
+      <p className="likes-item-price">{PRICE.toLocaleString()}원</p>
+    </div>
+  );
+};
+
+export default LikeItem;
