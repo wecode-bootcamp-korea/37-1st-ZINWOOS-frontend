6943825b2e587485eeebc292bb41e0084bf33b35
galee1025 <galee1025@gamil.com>
2022-09-29 22:16:52 +0900

    css 수정

--- src/pages/Likes/components/LikeItem.js
+++ src/pages/Likes/components/LikeItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LikeItem.scss';
 
 const LikeItem = ({ deleteBtn, likeData }) => {
@@ -7,19 +8,23 @@ const LikeItem = ({ deleteBtn, likeData }) => {
 
   return (
     <div className="LikeItem">
-      <div className="img-container">
-        <img src={detail_image} alt="관심상품 이미지" />
-        <div
-          className="like-out-btn"
-          id={item_id}
-          onClick={() => deleteBtn(item_id)}
-        >
-          x
-        </div>
+      <div
+        className="like-out-btn"
+        id={item_id}
+        onClick={() => deleteBtn(item_id)}
+      >
+        x
       </div>
 
-      <h2 className="likes-item-name">{name}</h2>
-      <p className="likes-item-price">{PRICE}원</p>
+      <div className="LikeItem-wrap">
+        <div className="img-container">
+          <img src={detail_image} alt="관심상품 이미지" />
+        </div>
+        <Link className="likes-item-link" to={`/product_detail/${item_id}`}>
+          <h2 className="likes-item-name">{name}</h2>
+          <p className="likes-item-price">{PRICE}원</p>
+        </Link>
+      </div>
     </div>
   );
 };
